a7c8bb0a471222546cc386716fef60d084e98434
jesskfulwood <jesskfulwood@gmail.com>
2018-07-30 17:18:45 +0100

    add apply and mask

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate num;
 extern crate ordered_float;
 
 use std::cell::RefCell;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
 use std::hash::Hash;
@@ -18,6 +19,7 @@ type StdResult<T, E> = std::result::Result<T, E>;
 type Result<T> = StdResult<T, failure::Error>;
 type IndexMap<T> = HashMap<T, Vec<usize>>;
 type Array<T> = Vec<T>;
+type Mask = Collection<bool>;
 
 // TODO "TypedFrame" with a custom-derive?
 // TODO Pretty-printing of DataFrame
@@ -115,7 +117,14 @@ pub struct Column {
     inner: Arc<ColumnInner>,
 }
 
+// TODO can we use a delegate macro here? much duplication
 impl Column {
+    fn from_inner(inner: ColumnInner) -> Self {
+        Column {
+            inner: Arc::new(inner),
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.inner.len()
     }
@@ -141,12 +150,63 @@ impl Column {
     pub fn sum(&self) -> f64 {
         self.inner.sum()
     }
+
+    fn mask<I>(&self, test: impl Fn(&I) -> bool) -> Mask
+    where
+        ColumnInner: DynamicMap<I, bool>,
+    {
+        // Example use:
+        // let mask = df["thing"].mask(|v| v > 10)
+        // TODO Macro:
+        // let mask = m!(df["thing"] > 10)
+        self.inner.mask(test)
+    }
+
+    fn apply_mask(&self, mask: &Mask) -> Column {
+        if mask.len() != self.len() {
+            panic!(
+                "Mask length doesn't match ({}, expected {})",
+                mask.len(),
+                self.len()
+            )
+        }
+        Column::from_inner(self.inner.apply_mask(mask))
+    }
+}
+
+macro_rules! impl_column_from {
+    ($fromenum:ident, $fromty:ty) => {
+        impl From<Array<$fromty>> for Column {
+            fn from(arr: Array<$fromty>) -> Column {
+                Column {
+                    inner: Arc::new(ColumnInner::$fromenum(Collection::new(arr))),
+                }
+            }
+        }
+    };
+}
+
+impl_column_from!(Int, i32);
+impl_column_from!(String, String);
+impl_column_from!(Bool, bool);
+// TODO this impl is no good, we want to pass simple f64s
+impl_column_from!(Float, OrderedFloat<f64>);
+
+impl From<Array<f64>> for Column {
+    fn from(arr: Array<f64>) -> Column {
+        // This looks very dangerous, but, OrderedFloat is just a newtype with no extra
+        // invariants, so it should be safe (prove me wrong?)
+        let arr = unsafe { std::mem::transmute::<_, Array<OrderedFloat<f64>>>(arr) };
+        Column {
+            inner: Arc::new(ColumnInner::Float(Collection::new(arr))),
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 enum ColumnInner {
     Bool(Collection<bool>),
-    I32(Collection<i32>),
+    Int(Collection<i32>),
     String(Collection<String>),
     Float(Collection<OrderedFloat<f64>>),
 }
@@ -158,7 +218,7 @@ macro_rules! column_apply {
             use ColumnInner::*;
             match self {
                   Bool(c) => $func(&c $(,$arg)*),
-                   I32(c) => $func(&c $(,$arg)*),
+                   Int(c) => $func(&c $(,$arg)*),
                 String(c) => $func(&c $(,$arg)*),
                  Float(c) => $func(&c $(,$arg)*),
             }
@@ -172,7 +232,7 @@ macro_rules! column_apply_pair {
             use ColumnInner::*;
             match (self, other) {
                 (Bool(c1), Bool(c2))     => $func(&c1, &c2 $(,$arg)*),
-                (I32(c1), I32(c2))       => $func(&c1, &c2 $(,$arg)*),
+                (Int(c1), Int(c2))       => $func(&c1, &c2 $(,$arg)*),
                 (String(c1), String(c2)) => $func(&c1, &c2 $(,$arg)*),
                 (Float(c1), Float(c2))   => $func(&c1, &c2 $(,$arg)*),
                 _ => panic!("Mismatching column types"),
@@ -191,7 +251,7 @@ impl ColumnInner {
         use ColumnInner::*;
         match self {
             Bool(c) => Bool(c.copy_locs(locs)),
-            I32(c) => I32(c.copy_locs(locs)),
+            Int(c) => Int(c.copy_locs(locs)),
             String(c) => String(c.copy_locs(locs)),
             Float(c) => Float(c.copy_locs(locs)),
         }
@@ -200,15 +260,28 @@ impl ColumnInner {
     fn matching_types(&self, other: &ColumnInner) -> bool {
         match (self, other) {
             (ColumnInner::Bool(_), ColumnInner::Bool(_)) => true,
-            (ColumnInner::I32(_), ColumnInner::I32(_)) => true,
+            (ColumnInner::Int(_), ColumnInner::Int(_)) => true,
             (ColumnInner::String(_), ColumnInner::String(_)) => true,
             _ => false,
         }
     }
 
-    fn sum(&self) -> f64 {
-        // How should this be done? Always f64? Precision a problem?
-        unimplemented!()
+    fn mask<I>(&self, test: impl Fn(&I) -> bool) -> Mask
+    where
+        ColumnInner: DynamicMap<I, bool>,
+    {
+        self.map(test)
+    }
+
+    fn apply_mask(&self, mask: &Mask) -> Self {
+        use ColumnInner::*;
+        match self {
+            // TODO be nice to call c.wrap()
+            Bool(c) => Bool(c.apply_mask(mask)),
+            Int(c) => Int(c.apply_mask(mask)),
+            String(c) => String(c.apply_mask(mask)),
+            Float(c) => Float(c.apply_mask(mask)),
+        }
     }
 }
 
@@ -219,6 +292,7 @@ struct Collection<T> {
 }
 
 impl<T: PartialEq> PartialEq for Collection<T> {
+    // We don't care if the indexes are the same
     fn eq(&self, other: &Self) -> bool {
         self.data == other.data
     }
@@ -249,6 +323,10 @@ impl<T> Collection<T> {
     fn iter(&self) -> impl Iterator<Item = &T> + '_ {
         self.data.iter()
     }
+
+    fn map<R>(&self, test: impl Fn(&T) -> R) -> Collection<R> {
+        Collection::new(self.iter().map(test).collect())
+    }
 }
 
 impl<T: Clone> Collection<T> {
@@ -256,6 +334,16 @@ impl<T: Clone> Collection<T> {
     fn copy_locs(&self, locs: &[usize]) -> Collection<T> {
         let data = locs.iter().map(|&ix| self.data[ix].clone()).collect();
         Collection::new(data)
+    }
+
+    fn apply_mask(&self, mask: &Mask) -> Self {
+        assert_eq!(self.len(), mask.len());
+        Collection::new(
+            self.iter()
+                .zip(mask.iter())
+                .filter_map(|(v, &b)| if b { Some(v.clone()) } else { None })
+                .collect(),
+        )
     }
 }
 
@@ -324,76 +412,86 @@ impl<T: Num + Copy + AsPrimitive<f64>> Collection<T> {
     }
 }
 
-macro_rules! impl_column_from {
-    ($fromenum:ident, $fromty:ty) => {
-        impl From<Array<$fromty>> for Column {
-            fn from(arr: Array<$fromty>) -> Column {
-                Column {
-                    inner: Arc::new(ColumnInner::$fromenum(Collection::new(arr))),
+trait DynamicMap<T, R> {
+    fn map(&self, f: impl Fn(&T) -> R) -> Collection<R>;
+}
+
+macro_rules! dynamic_apply_impl {
+    ($raw:ty, $enum:ident) => {
+        impl<R> DynamicMap<$raw, R> for ColumnInner {
+            fn map(&self, f: impl Fn(&$raw) -> R) -> Collection<R> {
+                use ColumnInner::*;
+                match self {
+                    $enum(c) => c.map(f),
+                    _ => panic!("Can't apply function to TODO column"),
                 }
             }
         }
     };
 }
 
-impl_column_from!(I32, i32);
-impl_column_from!(String, String);
-impl_column_from!(Bool, bool);
-// TODO this impl is no good, we want to pass simple f64s
-impl_column_from!(Float, OrderedFloat<f64>);
+dynamic_apply_impl!(i32, Int);
+dynamic_apply_impl!(bool, Bool);
+dynamic_apply_impl!(String, String);
 
-impl From<Array<f64>> for Column {
-    fn from(arr: Array<f64>) -> Column {
-        let arr = unsafe { std::mem::transmute::<_, Array<OrderedFloat<f64>>>(arr) };
-        Column {
-            inner: Arc::new(ColumnInner::Float(Collection::new(arr))),
-        }
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_basic_features() {
+        let mut df = DataFrame::new();
+        df.addcol("c1", vec![1, 2, 3, 4, 5]).unwrap();
+        df.addcol("c2", vec![2., 3., 4., 5., 6.]).unwrap();
+        df.addcol("c3", vec![true, true, false, true, false])
+            .unwrap();
+        let words: Vec<_> = "a b c d e".split(' ').map(String::from).collect();
+        df.addcol("c4", words).unwrap();
+
+        // test wrong length
+        let too_long = vec![1, 2, 3, 4, 5, 6];
+        df.addcol("cX", too_long).unwrap_err();
+        df.getcol("cX").unwrap_err();
+
+        assert_eq!(df.len(), 5);
+        assert_eq!(df.num_cols(), 4);
+
+        // index
+        let c1 = &df["c1"];
+        assert_eq!(c1.len(), 5);
     }
-}
 
-#[test]
-fn test_basic_features() {
-    let mut df = DataFrame::new();
-    df.addcol("c1", vec![1, 2, 3, 4, 5]).unwrap();
-    df.addcol("c2", vec![2., 3., 4., 5., 6.]).unwrap();
-    df.addcol("c3", vec![true, true, false, true, false])
-        .unwrap();
-    let words: Vec<_> = "a b c d e".split(' ').map(String::from).collect();
-    df.addcol("c4", words).unwrap();
+    #[test]
+    fn test_join() {
+        let mut df = DataFrame::new();
+        df.addcol("c1", vec![1, 2, 3, 4]).unwrap();
+        df.addcol("c2", vec![true, false, true, false]).unwrap();
 
-    // test wrong length
-    let too_long = vec![1, 2, 3, 4, 5, 6];
-    df.addcol("cX", too_long).unwrap_err();
-    df.getcol("cX").unwrap_err();
+        let mut df2 = DataFrame::new();
+        df2.addcol("c1", vec![2, 4, 3, 3, 2, 5]).unwrap();
+        let dfjoin = df.inner_join(&df2, "c1").unwrap();
 
-    assert_eq!(df.len(), 5);
-    assert_eq!(df.num_cols(), 4);
+        let c1 = dfjoin.getcol("c1").unwrap();
+        let e1 = Column::from(vec![2, 2, 3, 3, 4]);
+        assert_eq!(c1, &e1);
 
-    // index
-    let c1 = &df["c1"];
-    assert_eq!(c1.len(), 5);
-}
+        let c2 = dfjoin.getcol("c2").unwrap();
+        let e2 = Column::from(vec![false, false, true, true, false]);
+        assert_eq!(c2, &e2);
 
-#[test]
-fn test_join() {
-    let mut df = DataFrame::new();
-    df.addcol("c1", vec![1, 2, 3, 4]).unwrap();
-    df.addcol("c2", vec![true, false, true, false]).unwrap();
+        // Join on floats (is this wise?)
+        df.addcol("c3", vec![4., 3., 2., 1.]).unwrap();
+        df2.addcol("c3", vec![1., 0., 0., 0., 0., 0.]).unwrap();
+        let dfjoin2 = df.inner_join(&df2, "c3").unwrap();
+        assert_eq!(dfjoin2.len(), 1);
+    }
 
-    let mut df2 = DataFrame::new();
-    df2.addcol("c1", vec![2, 4, 3, 3, 2, 5]).unwrap();
-    let dfjoin = df.inner_join(&df2, "c1").unwrap();
-
-    let c1 = dfjoin.getcol("c1").unwrap();
-    let e1 = Column::from(vec![2, 2, 3, 3, 4]);
-    assert_eq!(c1, &e1);
-
-    let c2 = dfjoin.getcol("c2").unwrap();
-    let e2 = Column::from(vec![false, false, true, true, false]);
-    assert_eq!(c2, &e2);
-
-    df.addcol("c3", vec![4., 3., 2., 1.]).unwrap();
-    df2.addcol("c3", vec![1., 0., 0., 0., 0., 0.]).unwrap();
-    let dfjoin2 = df.inner_join(&df2, "c3").unwrap();
-    assert_eq!(dfjoin2.len(), 1);
+    #[test]
+    fn test_mask() {
+        let mut df = DataFrame::new();
+        df.addcol("c1", vec![1, 2, 3, 4]).unwrap();
+        let mask = df["c1"].mask(|&v: &i32| v > 2);
+        let cfilt = df["c1"].apply_mask(&mask);
+        assert_eq!(cfilt, Column::from(vec![3, 4]))
+    }
 }
