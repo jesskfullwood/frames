840c238b50e04bc9077a92398f1a85e2a1596947
jesskfulwood <jesskfulwood@gmail.com>
2018-08-19 15:00:08 +0100

    improve benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -46,23 +46,24 @@ mod benchmarks {
     use frame::Frame3;
     use test::Bencher;
 
+    const SIZE: i64 = 50_000;
+
     #[bench]
     fn bench_create_frame(b: &mut Bencher) {
         b.iter(|| {
             let f: Frame3<IntT, FloatT, BoolT> = frame![
-                col![1, 2, 3, 4],
-                col![1., 2., 3., 4.],
-                col![true, false, true, false],
+                Column::new((0..SIZE).map(Some)),
+                Column::new((0..SIZE).map(|v| v as f64).map(Some)),
+                Column::new((0..SIZE).map(|v| v % 2 == 0).map(Some)),
             ];
             f
         })
     }
 
-    const IXSIZE: i64 = 50_000;
 
     #[bench]
     fn bench_index_column_sequence(b: &mut Bencher) {
-        let mut c = Column::new((0..IXSIZE).map(Some));
+        let mut c = Column::new((0..SIZE).map(Some));
         b.iter(|| {
             c.build_index();
             c.drop_index();
@@ -73,7 +74,7 @@ mod benchmarks {
     fn bench_index_column_random_with_10_dupes(b: &mut Bencher) {
         use rand::Rng;
         let mut rng = rand::thread_rng();
-        let mut c = Column::new((0..IXSIZE).map(|_| rng.gen_range(0, IXSIZE / 10)).map(Some));
+        let mut c = Column::new((0..SIZE).map(|_| rng.gen_range(0, SIZE / 10)).map(Some));
         b.iter(|| {
             c.build_index();
             c.drop_index();
@@ -82,7 +83,7 @@ mod benchmarks {
 
     #[bench]
     fn bench_inner_join(b: &mut Bencher) {
-        let f1 = Frame::with((0..IXSIZE).map(Some));
+        let f1 = Frame::with((0..SIZE).map(Some));
         f1.get(IntT).build_index();
         let f2 = f1.clone();
         b.iter(|| f1.clone().inner_join(&f2, IntT, IntT))
